331dbf2c0f2723caee15559a279f604b6bb3c229
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2026-06-06 14:16:34 +0800

    feat: add fetch product by id test.

--- test/Product.Infrastructure.Tests/MySqlProductRepositoryTests.cs
+++ test/Product.Infrastructure.Tests/MySqlProductRepositoryTests.cs
@@ -7,6 +7,7 @@ namespace Product.Infrastructure.Tests
     {
         private const string TestConnectionString =
         "Server=localhost;Port=13306;Database=ProductDB;Uid=root;Pwd=MyStrongPass123!;";
+        private Guid TestProductId = Guid.Parse("11111111-1111-1111-1111-111111111111");
 
         [TestMethod]
         public async Task GetAllProducts_ShouldReturnAllProducts()
@@ -17,6 +18,17 @@ namespace Product.Infrastructure.Tests
             var products = await repository.GetAllProductsAsync();
 
             Assert.IsTrue(products.Any());
+        }
+
+        [TestMethod]
+        public async Task GetProductById_ShouldReturnProdcut() {
+            var factory = new MySqlConnectionFactory(TestConnectionString);
+            var repository = new RawSqlProductRepository(factory);
+
+            var product = await repository.GetProductByIdAsync(TestProductId);
+
+            Assert.IsNotNull(product);
+            Assert.AreEqual(product.Id, TestProductId);
         }
     }
 }
